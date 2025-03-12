79dc252e3219aaf9a199d954d17b841db9af92bf
John Caruthers <john.d.caruthers@gmail.com>
2025-03-11 22:44:35 -0400

    Create memory_os.py

---
+++ memory_os.py
@@ -0,0 +1,23 @@
+class Register_Mem:
+    # Register memory
+    pass
+
+class Cache_Mem:
+    # Cache memory
+    pass
+
+class Main_Mem:
+    # RAM
+    pass
+class Disk_Mem:
+    # Disk Memory
+    pass
+
+def load_mem():
+    pass
+
+def search_mem():
+    pass
+
+def defragment_mem():
+    pass
