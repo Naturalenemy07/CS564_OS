6f95ae16ab54dcccc3012188dbe8411bd67d5f03
John Caruthers <john.d.caruthers@gmail.com>
2025-03-16 22:32:33 -0400

    Update memory_os.py

--- memory_os.py
+++ memory_os.py
@@ -3,21 +3,43 @@ class Register:
     def __init__(self, size = 8):
         self.memory = [0] * size
 
+    def read(self, address):
+        """Reads value at address and returns"""
+        return self.memory[address]
+
+    def write(self, value, address):
+        """Writes a value at a designated position"""
+        self.memory[address] = value
+
 class Cache:
     # Cache memory
     def __init__(self, size = 1024):
         self.memory = [0] * size
+
+    def read(self, address):
+        """Reads value at address and returns"""
+        return self.memory[address]
+
+    def write():
+        pass
 
 class Main:
     # RAM
     def __init__(self, size = 1048576):
         self.memory = [0] * size
 
-def load_mem():
-    pass
+    def read(self, address):
+        """Reads value at address and returns"""
+        return self.memory[address]
 
-def search_mem():
-    pass
+    def write():
+        pass
 
-def defragment_mem():
-    pass
+    def defragment_mem():
+        pass
+
+r1 = Register()
+r1.write(10,0)
+r1.write(20,4)
+for i in range(8):
+    print(r1.read(i))
