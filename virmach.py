94c928ea5376b64da30fbc557debf9a56533601c
John Caruthers <john.d.caruthers@gmail.com>
2025-03-16 22:10:45 -0400

    Create virmach.py

---
+++ virmach.py
@@ -0,0 +1,11 @@
+# Runs the virtual operating system vOS
+
+class VirtualMachine:
+    """
+    Creates a completetly virtual operating system.
+
+
+    """
+
+    def __init__(self):
+        self.pc = 0 # Program counter
